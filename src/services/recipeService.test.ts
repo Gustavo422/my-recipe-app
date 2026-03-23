0602b6b159cbceca77a5fe4f321f0c5e18df5340
Gustavo De Moura <96140566+Gustavo422@users.noreply.github.com>
2026-03-23 02:55:34 +0000

    chore: Create the function `getRecipesByDifficulty` and add tests

--- src/services/recipeService.test.ts
+++ src/services/recipeService.test.ts
@@ -8,3 +8,10 @@ test("Integration test should be return an object", async () => {
   const recipeList = await recipeService();
   expect(typeof recipeList).toBe("object");
 });
+
+test("Function should return filtered recipe list", async () => {
+  const recipesArray = await recipeService();
+  const difficulty = "Medium";
+  const filteredRecipes = getRecipesByDifficulty(recipesArray, difficulty);
+  expect(typeof filteredRecipes).toBe("object");
+});
--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -1,13 +1,30 @@
-export async function recipeService() {
-  let recipes = {};
+export type Recipe = {
+  difficulty: string;
+};
+
+export type ApiResponse = {
+  recipes: Recipe[];
+};
+
+export async function recipeService(): Promise<ApiResponse | undefined> {
+  let recipesList = undefined;
   try {
     const req = await fetch("https://dummyjson.com/recipes");
-    recipes = await req.json();
-    return recipes;
+    recipesList = await req.json();
+    return recipesList;
   } catch (error) {
     console.error(error);
-    return recipes;
+    return recipesList;
   }
 }
 
-export function getRecipesByDifficulty(recipes: object, difficulty: string) {}
+export function getRecipesByDifficulty(
+  recipesObj: ApiResponse | undefined,
+  difficulty: string,
+) {
+  const diff = difficulty.toLowerCase();
+  const filteredRecipes = recipesObj?.recipes.filter(
+    (recipe) => recipe.difficulty?.toLowerCase() === diff,
+  );
+  return filteredRecipes?.length !== 0 ? filteredRecipes : undefined;
+}
